5994a51ef39b40ff2fffe6efbe17a788b4c92071
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-04-04 01:13:58 +0200

    Change style

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,10 +6,10 @@ return(
     <nav>
         <h2>Alaska Dream</h2>
         <button>Shop</button>
-        <button><span style={{fontSize:40+'px'}} className="material-symbols-outlined">
+        <button><span style={{fontSize:60+'px',fontWeight:700}} className="material-symbols-outlined">
 search
 </span></button>
-        <button><span style={{fontSize:40+'px'}} className="material-symbols-outlined">
+        <button><span style={{fontSize:60+'px',fontWeight:700}} className="material-symbols-outlined">
 shopping_basket
 </span></button>
     </nav>
