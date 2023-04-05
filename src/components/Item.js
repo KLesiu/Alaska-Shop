3ba5df5e365802168d53d10dd3f6e30d4191d264
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-04-05 17:31:40 +0200

    Add images to .items and create a space for informations

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,9 +2,23 @@ import React from "react";
 import './styles/Item.css'
 const Item=(props)=>{
     
+    const items=[{src:props.info.img.first},{src:props.info.img.second},{src:props.info.img.third}]
+   const generate= items.map((element)=>{
+    return(
+        <div className="item">
+            <img src={element.src}></img>
+        </div>
+    )
+        
+    })
 return(
-    <div className="item">
+    <div className="items">
         <h2>{props.info.name}</h2>
+        <div className="holderForInfo">
+            <h3>Informacje</h3>
+        </div>
+        <div className="holderForItems">{generate}
+        </div>
         
     </div>
 )
--- src/components/ShopMain.js
+++ src/components/ShopMain.js
@@ -4,9 +4,9 @@ import Item from "./Item";
 import { useState } from "react";
 const ShopMain=()=>{
     const [item,setItem]=useState({name:'Visit beautiful Juneau!',img:{
-        one:'public/Images/Juneau/1.jpg',
-        second:'public/Images/Juneau/2.jpg',
-        third: 'public/Images/Juneau/3.jpg'
+        first:'/Images/Juneau/1.jpg',
+        second:'/Images/Juneau/2.jpg',
+        third: '/Images/Juneau/3.jpg'
     }})
     return(
         <div className="containerShop">
@@ -15,48 +15,51 @@ const ShopMain=()=>{
             <ul>
               <li onClick={()=>{
                 setItem({name:'Visit beautiful Juneau!',img:{
-                    one:'public/Images/Juneau/1.jpg',
-                    second:'public/Images/Juneau/2.jpg',
-                    third: 'public/Images/Juneau/3.jpg'
+                    first:'/Images/Juneau/1.jpg',
+                    second:'/Images/Juneau/2.jpg',
+                    third: '/Images/Juneau/3.jpg'
                 }})
               }}>Trips to the Capital - Exciting Juneau</li> 
 
               <li onClick={()=>{
                 setItem({name:'Have an amazing adventures in Anchorage!',img:{
-                    one:'public/Images/Anchorage/boat-3536807__340.jpg',
-                    second:'public/Images/Anchorage/sandspollen-3767414__340.jpg',
-                    third: 'public/Images/Anchorage/venice-2933666__340.jpg'
+                    first:'/Images/Anchorage/boat-3536807__340.jpg',
+                    second:'/Images/Anchorage/sandspollen-3767414__340.jpg',
+                    third: '/Images/Anchorage/venice-2933666__340.jpg'
                 }})
               }}>Visit Anchorage and see the most popular places </li>
 
               <li onClick={()=>{
                 setItem({name:'Do you like sailing? You can do that in Alaska!',img:{
-                    one:'public/Images/ShipTrips/cruise-g86757c4ac_640.jpg',
-                    second:'public/Images/ShipTrips/boat-gd014cea71_640.jpg',
-                    third: ''
+                    first:'/Images/ShipTrips/cruise-g86757c4ac_640.jpg',
+                    second:'/Images/ShipTrips/boat-gd014cea71_640.jpg',
+                    third: '/Images/whale.jpg'
+                    
                 }})
               }} >Enjoy Ship Trips</li>
 
               <li onClick={()=>{
-                setItem({name:'Enjoy Alaska alone,family or friends. Rent a motorhome!',img:{
-                    one:'public/Images/camper.jpg',
-                    second:'',
-                    third: ''
+                setItem({name:'Enjoy Alaska alone,family or friends. Rent a motorhome or move by train',img:{
+                    first:'/Images/camper.jpg',
+                    second: '/Images/train.jpg',
+                    third: '/Images/Motorhome/camper-ge2cb69f63_640.jpg'
+                    
                 }})
               }}>Rent a motorhome and admire Alaska landscape</li>
 
               <li onClick={()=>{
                 setItem({name:'Enjoy the beauty of nature in National Denali Park',img:{
-                    one:'public/Images/Denali/denali-national-park-g32f0dae85_640.jpg',
-                    second:'public/Images/Denali/marmot-g2a413b880_640.jpg',
-                    third: 'public/Images/Denali/moose-g4e34e03c5_640.jpg'
+                    first:'/Images/Denali/denali-national-park-g32f0dae85_640.jpg',
+                    second:'/Images/Denali/marmot-g2a413b880_640.jpg',
+                    third: '/Images/Denali/moose-g4e34e03c5_640.jpg'
                 }})
               }}>Trips to Denali Nation Park with a guide</li>
 
               <li onClick={()=>{
                 setItem({name:'Fishing is pure pleasure with us, if you need equipment, you will find it with us!',img:{
-                    one:'public/Images/Fishing/boat-ga1168822d_640.jpg',
-                    two:'public/Images/Fishing/fishing-g10e0ed3b9_640.jpg'
+                    first:'/Images/Fishing/boat-ga1168822d_640.jpg',
+                    second:'/Images/Fishing/fishing-g10e0ed3b9_640.jpg',
+                    third: '/Images/Fishing/rod-g7a2558edb_640.jpg'
                 }})
               }}>Rent a fishing equipment</li>
             </ul>
