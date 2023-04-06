019ea555a3e76333c6713660ad9997075becd414
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-04-06 18:10:19 +0200

    Add attractions list

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,6 +3,7 @@ import './styles/Item.css'
 const Item=(props)=>{
     
     const items=[{src:props.info.img.first},{src:props.info.img.second},{src:props.info.img.third}]
+    const atractions= [{name:props.info.atractions.first.name},{name:props.info.atractions.second.name},{name:props.info.atractions.third.name}]
    const generate= items.map((element)=>{
     return(
         <div className="item">
@@ -11,11 +12,19 @@ const Item=(props)=>{
     )
         
     })
+    const showAtractions=atractions.map((element)=>{
+        return(
+            <li>{element.name}</li>
+        )
+    })
 return(
     <div className="items">
         <h2>{props.info.name}</h2>
         <div className="holderForInfo">
-            <h3>Informacje</h3>
+            <h3>Atractions:</h3>
+            <ul className="listAtractions">
+                {showAtractions}
+            </ul>
         </div>
         <div className="holderForItems">{generate}
         </div>
