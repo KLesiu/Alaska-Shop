3408a88b4832ddc09b8616910e3011a1a86ac799
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-04-08 22:32:40 +0200

    Add price of all attractions

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,18 +1,30 @@
 import React from "react";
+
 import './styles/Basket.css'
 const Basket=(props)=>{
+    
     const list=props.basket.list.map((element)=>{
         return(
             <li>{element}</li>
         )
     })
+    
+     
+     
+
+    
+   
+    
+   
+    
 return(
     <div className="basketDiv">
         <h2>Your basket</h2>
-        <button onClick={props.hideBasket}>X</button>
-        <ul>
+        <button className="closeBasket" onClick={props.hideBasket}>X</button>
+        <ul className="basketList">
             {list}
         </ul>
+        <p>{props.basket.prices}</p>
     </div>
 )
 }
