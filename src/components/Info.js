a0c9c08612062b68ef06de18757ab0fd21d5267b
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-04-09 21:55:19 +0200

    Optimize for mobiles devices

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './styles/Info.css'
+import { Link } from "react-router-dom";
 const Info=()=>{
 return(
     <div className="container">
        <div className="infoHolder">   <h1>Visit Alaska now</h1>
         <p>In our shop you can buy amazing travels to the most exciting places in Alaska.</p></div>
-        <button className="seeOffer">See offer</button>
+       <Link to={'/shopPage'}> <button className="seeOffer">See offer</button></Link>
         <div className="background"></div>
     </div>
 )
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import './styles/Main.css'
 
 const Main=()=>{
     return(
-        <video width="100%" height='100%' src='/Videos/alaskaVideo.mp4' type="video/mp4" loop autoPlay muted>
+        <video src='/Videos/alaskaVideo.mp4' type="video/mp4" loop autoPlay muted>
       </video>
     )
 }
