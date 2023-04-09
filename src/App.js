b635a65af3ddf34730068d899cdd0829dd4c43a2
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-04-10 00:06:22 +0200

    Change paths

--- src/App.js
+++ src/App.js
@@ -148,9 +148,9 @@ const hideBasket=()=>{
             <ul>
               <li onClick={()=>{
                 setItem({name:'Visit beautiful Juneau!',img:{
-   first:'/Images/Juneau/1.jpg',
-   second:'/Images/Juneau/2.jpg',
-   third: '/Images/Juneau/3.jpg'
+   first:'./Images/Juneau/1.jpg',
+   second:'./Images/Juneau/2.jpg',
+   third: './Images/Juneau/3.jpg'
                 },atractions:{
  first: {
    name:'Juneau and Mendenhall glacier',
@@ -177,9 +177,9 @@ const hideBasket=()=>{
           }})
               }}>Trips to the Capital - Exciting Juneau</li>               <li onClick={()=>{
                 setItem({name:'Have an amazing adventures in Anchorage!',img:{
-   first:'/Images/Anchorage/boat-3536807__340.jpg',
-   second:'/Images/Anchorage/sandspollen-3767414__340.jpg',
-   third: '/Images/Anchorage/dog-g30058c7ad_640.jpg'
+   first:'./Images/Anchorage/boat-3536807__340.jpg',
+   second:'./Images/Anchorage/sandspollen-3767414__340.jpg',
+   third: './Images/Anchorage/dog-g30058c7ad_640.jpg'
                 },
               atractions:{
                 first: {
@@ -202,9 +202,9 @@ const hideBasket=()=>{
               }})
               }}>Visit Anchorage and see the most popular places </li>              <li onClick={()=>{
                 setItem({name:'Do you like sailing? You can do that in Alaska!',img:{
-   first:'/Images/ShipTrips/cruise-g86757c4ac_640.jpg',
-   second:'/Images/ShipTrips/boat-gd014cea71_640.jpg',
-   third: '/Images/whale.jpg'
+   first:'./Images/ShipTrips/cruise-g86757c4ac_640.jpg',
+   second:'./Images/ShipTrips/boat-gd014cea71_640.jpg',
+   third: './Images/whale.jpg'
                 },
               atractions: {
                 first: {
@@ -226,9 +226,9 @@ const hideBasket=()=>{
               }})
               }} >Enjoy Ship Trips</li>              <li onClick={()=>{
                 setItem({name:'Enjoy Alaska alone,family or friends. Rent a motorhome or move by train',img:{
-   first:'/Images/camper.jpg',
-   second: '/Images/train.jpg',
-   third: '/Images/Motorhome/camper-ge2cb69f63_640.jpg'
+   first:'./Images/camper.jpg',
+   second: './Images/train.jpg',
+   third: './Images/Motorhome/camper-ge2cb69f63_640.jpg'
                 },
               atractions:{
                 first:{
@@ -251,9 +251,9 @@ const hideBasket=()=>{
               }})
               }}>Rent a motorhome and admire Alaska landscape</li>              <li onClick={()=>{
                 setItem({name:'Enjoy the beauty of nature in National Denali Park',img:{
-   first:'/Images/Denali/denali-national-park-g32f0dae85_640.jpg',
-   second:'/Images/Denali/marmot-g2a413b880_640.jpg',
-   third: '/Images/Denali/moose-g4e34e03c5_640.jpg'
+   first:'./Images/Denali/denali-national-park-g32f0dae85_640.jpg',
+   second:'./Images/Denali/marmot-g2a413b880_640.jpg',
+   third: './Images/Denali/moose-g4e34e03c5_640.jpg'
                 },atractions:{
  first: {
    name:'Denali Park Zipline Adventure',
@@ -275,9 +275,9 @@ const hideBasket=()=>{
                 }})
               }}>Trips to Denali Nation Park with a guide</li>              <li onClick={()=>{
                 setItem({name:'Fishing is pure pleasure with us, if you need equipment, you will find it with us!',img:{
-   first:'/Images/Fishing/boat-ga1168822d_640.jpg',
-   second:'/Images/Fishing/fishing-g10e0ed3b9_640.jpg',
-   third: '/Images/Fishing/rod-g7a2558edb_640.jpg'
+   first:'./Images/Fishing/boat-ga1168822d_640.jpg',
+   second:'./Images/Fishing/fishing-g10e0ed3b9_640.jpg',
+   third: './Images/Fishing/rod-g7a2558edb_640.jpg'
                 },
               atractions:{
                 first: {
@@ -318,9 +318,9 @@ const hideBasket=()=>{
             <ul>
              <Link to={'/shopPage'}><li onClick={()=>{
                 setItem({name:'Visit beautiful Juneau!',img:{
-   first:'/Images/Juneau/1.jpg',
-   second:'/Images/Juneau/2.jpg',
-   third: '/Images/Juneau/3.jpg'
+   first:'./Images/Juneau/1.jpg',
+   second:'./Images/Juneau/2.jpg',
+   third: './Images/Juneau/3.jpg'
                 },atractions:{
  first: {
    name:'Juneau and Mendenhall glacier',
@@ -345,9 +345,9 @@ const hideBasket=()=>{
           }})
               }}>Trips to the Capital - Exciting Juneau</li> </Link>             <Link to={'/shopPage'}> <li onClick={()=>{
                 setItem({name:'Have an amazing adventures in Anchorage!',img:{
-   first:'/Images/Anchorage/boat-3536807__340.jpg',
-   second:'/Images/Anchorage/sandspollen-3767414__340.jpg',
-   third: '/Images/Anchorage/venice-2933666__340.jpg'
+   first:'./Images/Anchorage/boat-3536807__340.jpg',
+   second:'./Images/Anchorage/sandspollen-3767414__340.jpg',
+   third: './Images/Anchorage/venice-2933666__340.jpg'
                 },
               atractions:{
                 first: {
@@ -373,9 +373,9 @@ const hideBasket=()=>{
               }})
               }}>Visit Anchorage and see the most popular places </li> </Link>             <Link to={'/shopPage'}> <li onClick={()=>{
                 setItem({name:'Do you like sailing? You can do that in Alaska!',img:{
-   first:'/Images/ShipTrips/cruise-g86757c4ac_640.jpg',
-   second:'/Images/ShipTrips/boat-gd014cea71_640.jpg',
-   third: '/Images/whale.jpg'
+   first:'./Images/ShipTrips/cruise-g86757c4ac_640.jpg',
+   second:'./Images/ShipTrips/boat-gd014cea71_640.jpg',
+   third: './Images/whale.jpg'
                 },
               atractions: {
                 first: {
@@ -401,9 +401,9 @@ const hideBasket=()=>{
               }})
               }} >Enjoy Ship Trips</li> </Link>             <Link to={'/shopPage'}> <li onClick={()=>{
                 setItem({name:'Enjoy Alaska alone,family or friends. Rent a motorhome or move by train',img:{
-   first:'/Images/camper.jpg',
-   second: '/Images/train.jpg',
-   third: '/Images/Motorhome/camper-ge2cb69f63_640.jpg'
+   first:'./Images/camper.jpg',
+   second: './Images/train.jpg',
+   third: './Images/Motorhome/camper-ge2cb69f63_640.jpg'
                 },
               atractions:{
                 first:{
@@ -426,9 +426,9 @@ const hideBasket=()=>{
               }})
               }}>Rent a motorhome and admire Alaska landscape</li> </Link>            <Link to={'/shopPage'}> <li onClick={()=>{
                 setItem({name:'Enjoy the beauty of nature in National Denali Park',img:{
-   first:'/Images/Denali/denali-national-park-g32f0dae85_640.jpg',
-   second:'/Images/Denali/marmot-g2a413b880_640.jpg',
-   third: '/Images/Denali/moose-g4e34e03c5_640.jpg'
+   first:'./Images/Denali/denali-national-park-g32f0dae85_640.jpg',
+   second:'./Images/Denali/marmot-g2a413b880_640.jpg',
+   third: './Images/Denali/moose-g4e34e03c5_640.jpg'
                 },atractions:{
  first: {
    name:'Denali Park Zipline Adventure',
@@ -450,9 +450,9 @@ const hideBasket=()=>{
                 }})
               }}>Trips to Denali Nation Park with a guide</li> </Link>            <Link to={'/shopPage'}><li onClick={()=>{
                 setItem({name:'Fishing is pure pleasure with us, if you need equipment, you will find it with us!',img:{
-   first:'/Images/Fishing/boat-ga1168822d_640.jpg',
-   second:'/Images/Fishing/fishing-g10e0ed3b9_640.jpg',
-   third: '/Images/Fishing/rod-g7a2558edb_640.jpg'
+   first:'./Images/Fishing/boat-ga1168822d_640.jpg',
+   second:'./Images/Fishing/fishing-g10e0ed3b9_640.jpg',
+   third: './Images/Fishing/rod-g7a2558edb_640.jpg'
                 },
               atractions:{
                 first: {
